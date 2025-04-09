d29dc86ea934298d4790fee0286347d356307d5d
lolcat245 <janazamachajeva@seznam.cz>
2025-04-09 21:11:22 +0200

    doplneno po lekci

---
+++ lekce_4.py
@@ -0,0 +1,28 @@
+# print("uiiauuuiiiai")
+# print("silly")
+# print("miau")
+
+# cisla = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+# for cislo in cisla:
+#     if cislo % 3 == 0 and cislo % 5 == 0:
+#         print("FizzBuzz")
+#     elif cislo % 3 == 0:
+#         print("Fizz")
+#     elif cislo % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(cislo)
+
+# content = [
+#     ['jmeno;prijmeni;email;projekt', 'plat'],
+#     ['Matous;Holinka;m.holinka@firma.cz;hr', '1000'],
+#     ['Petr;Svetr;p.svetr@firma.cz;devops','2000']
+# ]
+# for item in content:
+#     for subitem in item:
+#         for subsubitem in subitem.split(";"):
+#             print(subsubitem)
+
+a = ['a', 'b', 'c']
+print(*a)
+print(a[0], a[1], a[2])
